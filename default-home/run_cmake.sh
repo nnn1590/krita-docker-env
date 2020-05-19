1ac720818c2b4a83b3c3211569398e255d5cee9f
Dmitry Kazakov <dimula73@gmail.com>
2020-05-19 16:16:19 +0300

    Make docker env build enable Q_ASSERT

--- default-home/run_cmake.sh
+++ default-home/run_cmake.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cmake -DCMAKE_INSTALL_PREFIX=${KRITADIR} \
-      -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+      -DCMAKE_BUILD_TYPE=Debug \
+      -DKRITA_DEVS=ON \
       -DBUILD_TESTING=TRUE \
       -DHIDE_SAFE_ASSERTS=FALSE \
       -DPYQT_SIP_DIR_OVERRIDE=~/appimage-workspace/deps/usr/share/sip \
