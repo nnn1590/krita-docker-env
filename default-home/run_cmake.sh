398fd1220cde8865d266a0173640a1d1f0e4c657
NNN1590 <nnn1590@nnn1590.org>
2020-10-26 23:04:17 +0900

    run_cmake.sh: Change to build release-build

--- default-home/run_cmake.sh
+++ default-home/run_cmake.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cmake -DCMAKE_INSTALL_PREFIX=${KRITADIR} \
-      -DCMAKE_BUILD_TYPE=Debug \
-      -DKRITA_DEVS=ON \
-      -DBUILD_TESTING=TRUE \
-      -DHIDE_SAFE_ASSERTS=FALSE \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DKRITA_DEVS=OFF \
+      -DBUILD_TESTING=FALSE \
+      -DHIDE_SAFE_ASSERTS=TRUE \
       -DPYQT_SIP_DIR_OVERRIDE=~/appimage-workspace/deps/usr/share/sip \
       $@
